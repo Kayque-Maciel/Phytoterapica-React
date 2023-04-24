db1e2a2e841373c9466dcbda3f3846e652c97620
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-04-24 09:03:50 -0300

    Card de filtragem finalizado

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -1,11 +1,17 @@
 import styles from "../Components/css/Produtos.module.css"
 import filtro from "../Components/images/filtro.png"
+import ordenar
 
 function Produtos(){
     return(
         <div>
            <div className={styles.Filtro}>
                 <p>FILTRAR</p>
+                <img src={filtro} alt="" />
+           </div>
+           
+           <div className={styles.Filtro}>
+                <p>ORDENAR POR</p>
                 <img src={filtro} alt="" />
            </div>
         </div>
